8fee956caad9838551000764d6fef2768a8a6338
Sam <iaiznura@gmail.com>
2023-02-19 21:53:48 +0300

    done homework 1

--- module_datastruct/utils.py
+++ module_datastruct/utils.py
@@ -1,1 +1,23 @@
-print("hello")
+class Node:
+
+    def __init__(self, data, prev):
+        self.data = data
+        self.prev = prev
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        new_node = Node(data)
+        new_node.prev = self.top
+        self.top = new_node
+
+
+
+n1 = Node(5, None)
+n2 = Node('a', n1)
+print(n1.data)
+print(n2.data)
+print(n1)
