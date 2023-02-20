3a2cff0d3a9c216620d369fb57012fbea896aa51
Sam <iaiznura@gmail.com>
2023-02-20 18:23:53 +0300

    add tests

--- module_datastruct/utils.py
+++ module_datastruct/utils.py
@@ -1,19 +1,18 @@
 class Node:
 
-    def __init__(self, data, prev):
+    def __init__(self, data, top=None):
         self.data = data
-        self.prev = prev
+        self.top = top
 
 
 class Stack:
-    def __init__(self):
-        self.top = None
+    def __init__(self, top=None):
+        self.top = top
 
     def push(self, data):
-        new_node = Node(data)
-        new_node.prev = self.top
+        new_node = Node(data, None)
+        new_node.top = self.top
         self.top = new_node
-
 
 
 n1 = Node(5, None)
@@ -21,3 +20,6 @@ n2 = Node('a', n1)
 print(n1.data)
 print(n2.data)
 print(n1)
+stack = Stack(n2)
+stack.push('data3')
+print(stack.top)
---
+++ tests/test_utils.py
@@ -0,0 +1,21 @@
+import unittest
+
+from module_datastruct.utils import Node, Stack
+
+node1 = Node("data1")
+node2 = Node('data2', node1)
+node2.top = node1
+stack = Stack(node2)
+stack.push("data3")
+stack.push("data4")
+stack.push("data5")
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_utils(self):
+        self.assertEqual(node1.top, None)
+        self.assertEqual(node1.data, "data1")
+        self.assertEqual(stack.top.data, "data5")
+        self.assertEqual(stack.top.top.top.top, node2)
+        self.assertEqual(stack.top.top.top.top.top.data, "data1")
