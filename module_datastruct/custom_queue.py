adee30481dbb494fc1a2a88496129a642de9e099
Sam <iaiznura@gmail.com>
2023-02-28 22:08:49 +0300

    done homework 4

--- module_datastruct/custom_queue.py
+++ module_datastruct/custom_queue.py
@@ -21,17 +21,34 @@ class Queue:
             self.tail.next_node = new_node
             self.tail = new_node
 
-    # def dequeue(self):
-    #     """Удалить элемент из начала очереди."""
+    def dequeue(self):
+        """Удалить элемент из начала очереди."""
+        if self.head is not None:
+            remove = self.head
+            self.head = self.head.next_node
+            return remove.data
+        else:
+            return None
 
 
+# Homework 3
+# queue = Queue()
+# queue.enqueue('data1')
+# queue.enqueue('data2')
+# queue.enqueue('data3')
+#
+# print(queue.head.data)
+# print(queue.head.next_node.data)
+# print(queue.tail.data)
+# print(queue.tail.next_node)
+# print(queue.tail.next_node.data)
+
+# Homework 4
 queue = Queue()
 queue.enqueue('data1')
 queue.enqueue('data2')
 queue.enqueue('data3')
-
-print(queue.head.data)
-print(queue.head.next_node.data)
-print(queue.tail.data)
-print(queue.tail.next_node)
-print(queue.tail.next_node.data)
+print(queue.dequeue())
+print(queue.dequeue())
+print(queue.dequeue())
+print(queue.dequeue())
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -35,3 +35,12 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(queue.head.data, "data1")
         self.assertEqual(queue.head.next_node.data, "data2")
 
+    def test_dequeue(self):
+        queue = Queue()
+        queue.enqueue("data1")
+        queue.enqueue("data2")
+        self.assertEqual(queue.dequeue(), "data1")
+        self.assertEqual(queue.dequeue(), "data2")
+        self.assertEqual(queue.dequeue(), None)
+
+
